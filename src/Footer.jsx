164be85450f0a3cb61d11393aa65e82d4f90a459
Janko <mailodjanka@gmail.com>
2020-02-17 19:38:08 +0100

    Added css framework for styling

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+      <div className="ui container">
+        <p id="footer">Made with React {React.version}</p>
+      </div>
+    );
+};
+
+export default Footer
