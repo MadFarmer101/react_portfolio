bcf17eaaf08c7c333a97b6244f6a9126897e650e
Janko <mailodjanka@gmail.com>
2020-02-19 13:02:16 +0100

    Added link to the title of projects cards

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -8,7 +8,8 @@ const ProjectCard = ({ project }) => {
                     <img src={project.image} />
                 </div>
                 <div class="content">
-                    <h3 class="ui header">{project.name}</h3>
+                    <h3 class="ui header"><a href={project.url}>{project.name}</a>
+                    </h3>
 
                     <div class="description">{project.description}</div>
                 </div>
