8cb2689301682c5e74cf037833b6694b50f3ba26
Janko <mailodjanka@gmail.com>
2020-02-20 08:50:24 +0100

    Added images to the front page

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Hello = () => {
     return (
-      <div className="ui main container">
-        <h1 id="hello">Hello World</h1>
+      <div className="ui main container" id="hello">
+          <div class="ui stackable two column grid">
+            <div className="column">
+              <img src="./src/images/birds.jpg" id="birds"/>
+            </div>
+            <div className="column">
+              <img src="./src/images/guy.png" id="guy"/>
+            </div>
+        </div>
       </div>
     );
 };
