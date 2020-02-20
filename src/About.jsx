13fe550a235477ea070e831a3f75618c1fcea691
Janko <mailodjanka@gmail.com>
2020-02-20 19:30:40 +0100

    Styled Abut header

--- src/About.jsx
+++ src/About.jsx
@@ -2,9 +2,26 @@ import React from "react"
 import { UndrawOnTheOffice } from "react-undraw-illustrations";
 
 const About = () => {
+    let audio = new Audio("./src/music/enjoy.mp3")
+
+    const start = () => {
+      audio.play()
+    }
+  
+    const pause = () => {
+      audio.pause()
+    }
     return (
         <div classname="ui main container">
-            <div class="ui stackable two column grid">
+            <div class="ui stackable three column grid">
+                <button class="ui labeled icon button" onClick={start}>
+                    <i class="play icon"></i>
+                        Play
+                </button>
+                <button class="ui right labeled icon button" onClick={pause}>
+                    <i class="pause icon"></i>
+                        Pause
+                </button>
                 <div class="column" id="ill-about">
                     <UndrawOnTheOffice primaryColor='#12283a' height='500px'/>
                 </div>
@@ -13,6 +30,7 @@ const About = () => {
                     <p>
                         Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?
                     </p>
+                    
                 </div>
             </div>
         </div>  
