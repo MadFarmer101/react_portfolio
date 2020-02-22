d5ff3629be84d9e97aa96f5ee6c90bb612323cde
Janko <mailodjanka@gmail.com>
2020-02-22 17:04:07 +0100

    Made only the image a visible part of the card, content is hidden

--- src/CvCard.jsx
+++ src/CvCard.jsx
@@ -4,13 +4,13 @@ const CvCard = ({ info }) => {
   return (
     <>
       <div class="ui card">
-        <div class="image">
-          <img src={info.image} />
-        </div>
-        <div class="content">
+        <div class="ui slide masked reveal image">
+          <img src={info.image} class="visible content" />
+          <div class="hidden content">
           <h3 class="ui header">{info.name}</h3>
-
-          <div class="description">{info.description}</div>
+          
+          <p class="descrition">{info.description}</p>
+          </div>
         </div>
       </div>
     </>
